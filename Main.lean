b0992858259af6fc4fb349d83a0c447eb8c2a58b
Lean 4 VS Code Extension <>
2025-06-09 11:06:20 -0400

    Initial commit

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import TfLean
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ TfLean.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `TfLean` library.
+-- Import modules here that should be built as part of the library.
+import TfLean.Basic
